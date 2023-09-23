9662fe54ce2af39e27bb47459d2f5650ba2d0713
Zhang Hong Ming <planc2333@outlook.com>
2023-09-23 02:16:24 +0000

    update

--- 200-USD-bot/news.py
+++ 200-USD-bot/news.py
@@ -6,11 +6,13 @@ import requests
 import botpy
 
 class Client(botpy.Client):
-    async def 
+    def hi(self):
+        print("hello")
 
 with open("./config.json", "r", encoding="UTF-8") as config_file:
     config = json.loads(config_file.read())
 
 intents = botpy.Intents(public_guild_messages=True, guild_messages=True, guild_members=True)
 client = Client(intents=intents)
-client.run(appid=config['AppID'], token=config['Token'])
+client.hi()
+coro = client.run(appid=config['AppID'], token=config['Token'], ret_coro=True)
